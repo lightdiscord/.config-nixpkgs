29cb093dbc5262c2ea795925fbdf89f6dbd46c56
LightDiscord <contact@lightdiscord.fr>
2017-11-26 18:41:57 +0100

    Add a Git alias

--- programs-config/git.nix
+++ programs-config/git.nix
@@ -6,4 +6,7 @@
     key = "2C811B29E5BA876E76283E4252BB9E642B4B24D2";
     signByDefault = true;
   };
+  aliases = {
+    "ls" = "log --graph --abbrev-commit --all --format=format:'%C(bold green)%h%C(reset) - %C(bold white)(%ar)%C(reset) %C(white)%s -%C(bold white) %an%C(reset)'";
+  };
 }
