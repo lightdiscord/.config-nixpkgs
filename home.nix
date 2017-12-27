9539608fdb02351b3a5d50ace35df695d9f26ae0
LightDiscord <contact@lightdiscord.fr>
2017-12-27 15:59:44 +0100

    Update git signing key

--- home.nix
+++ home.nix
@@ -3,13 +3,14 @@
 let
   polybar = pkgs.polybar.override { i3GapsSupport = true; githubSupport = true; mpdSupport = true; };
   neovim = pkgs.neovim.override { configure = (import ./neovim-config { inherit pkgs; }); };
-  hyper = import ./hyper-config { inherit (pkgs) stdenv hyper fetchurl; };
   python3 = pkgs.python36.withPackages (ps: [ ps.numpy ps.editorconfig ]);
   python2 = pkgs.python27.withPackages (ps: [ ps.numpy ps.editorconfig ]);
-  flashplayer = pkgs.lib.overrideDerivation pkgs.flashplayer (old: {
+  discord = pkgs.lib.overrideDerivation pkgs.discord (old: {
+    version = "0.0.3";
+    name = "discord-0.0.3";
     src = pkgs.fetchurl {
-      url = "https://fpdownload.adobe.com/get/flashplayer/pdc/27.0.0.187/flash_player_npapi_linux.x86_64.tar.gz";
-      sha256 = "1hfcphcvdam62k983rm6r42mnkih4nfwyrnx0v88z3nw14mjr4c3";
+      url = "https://dl.discordapp.net/apps/linux/0.0.3/discord-0.0.3.tar.gz";
+      sha256 = "1yxxy9q75zlgk1b4winw4zy9yxk5pn8x4camh52n6v3mw6gq0bfh";
     };
   });
   sysconfig = (import <nixpkgs/nixos> {}).config;
@@ -43,7 +44,6 @@ in
     rofi
     i3lock
     vlc
-    hyper
     alacritty
     gitkraken
     pavucontrol
@@ -55,7 +55,6 @@ in
     hal-flash
     openh264
     x264
-    flashplayer
     firefox-devedition-bin
     chromium
     tor
--- programs-config/git.nix
+++ programs-config/git.nix
@@ -3,7 +3,7 @@
   userName = "LightDiscord";
   userEmail = "contact@lightdiscord.fr";
   signing = {
-    key = "2C811B29E5BA876E76283E4252BB9E642B4B24D2";
+    key = "685CA08F30398537ACD08A53374A91189377ED82";
     signByDefault = true;
   };
   aliases = {
